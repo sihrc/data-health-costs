aec0750ba924866c4e2ac5bd36aede42b9d5e668
Chris <sihrc.c.lee@gmail.com>
2014-01-31 16:20:59 -0500

    Added datacode module with Data Handler

---
+++ src/analyze.py
@@ -0,0 +1,10 @@
+#Useful Libraries
+import numpy as np
+import pickle as p
+
+#Data Library
+import datacode.feature_dict as dc
+
+if __name__ == "__main__":
+	data = dc.Data(codebook = dc)
+	print data
