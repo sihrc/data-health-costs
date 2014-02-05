4ddf90c5ae5b45c61f1c4bf6a2f0ea15a0ba225f
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-02-04 22:05:20 -0500

    started working on visuals. feature vs cost has been implemented

--- src/visuals.py
+++ src/visuals.py
@@ -1,1 +1,26 @@
 #To-DO Create a Visuals module that holds all the visualization TASKS
+import matplotlib.pyplot as plt
+class Graphs():
+
+	def __init__ (self, data):
+		self.data = data
+		self.costId = data.lookUp(desc = "CHG")[0][0] # V49
+		self.cost = data.getColumn(self.costId)
+
+	def FeatureVsCost(self, var):
+			try:
+				data = self.data.getColumn(var)
+		 		plt.scatter(data, self.cost)
+		 		print "Plotting " + var + " vs cost plot"
+		 		plt.xlabel(self.data.lookUp(var = var)[0])
+		 		plt.ylabel("Cost in dollars")
+		 		plt.savefig("../visuals/feature_v_cost/" + self.data.lookUp(var = var)[0].replace(" ", "_") + ".png")
+			except:
+			 	print "Plotting " + str(i) + " failed"
+
+	def AllFeatureVsCost(self):
+		for i in range (len(self.data.features)):
+			try:
+				self.FeatureVsCost("V" + str(i))
+			except:
+				print "Plotting " + str(i) + " failed"
