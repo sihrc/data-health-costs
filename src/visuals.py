c2f9d5e95a7ec5651cad715e230d776aed1f00aa
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-02-07 11:30:15 -0500

    Changed visuals from being a class to a set of functions

--- src/visuals.py
+++ src/visuals.py
@@ -1,26 +1,21 @@
 #To-DO Create a Visuals module that holds all the visualization TASKS
 import matplotlib.pyplot as plt
-class Graphs():
 
-	def __init__ (self, data):
-		self.data = data
-		self.costId = data.lookUp(desc = "CHG")[0][0] # V49
-		self.cost = data.getColumn(self.costId)
 
-	def FeatureVsCost(self, var):
-			try:
-				data = self.data.getColumn(var)
-		 		plt.scatter(data, self.cost)
-		 		print "Plotting " + var + " vs cost plot"
-		 		plt.xlabel(self.data.lookUp(var = var)[0])
-		 		plt.ylabel("Cost in dollars")
-		 		plt.savefig("../visuals/feature_v_cost/" + self.data.lookUp(var = var)[0].replace(" ", "_") + ".png")
-			except:
-			 	print "Plotting " + str(i) + " failed"
+def FeatureVsCost(data, cost, var):
+		try:
+			data = data.getColumn(var)
+	 		plt.scatter(data, cost)
+	 		print "Plotting " + var + " vs cost plot"
+	 		plt.xlabel(data.lookUp(var = var)[0])
+	 		plt.ylabel("Cost in dollars")
+	 		plt.savefig("../visuals/feature_v_cost/" + data.lookUp(var = var)[0].replace(" ", "_") + ".png")
+		except:
+		 	print "Plotting " + str(i) + " failed"
 
-	def AllFeatureVsCost(self):
-		for i in range (len(self.data.features)):
-			try:
-				self.FeatureVsCost("V" + str(i))
-			except:
-				print "Plotting " + str(i) + " failed"
+def AllFeatureVsCost(data):
+	for i in range (len(data.features)):
+		try:
+			FeatureVsCost("V" + str(i))
+		except:
+			print "Plotting " + str(i) + " failed"
