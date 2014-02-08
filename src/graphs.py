b388846b6561491347160247f7d3765fd014172c
CJ Fogel <fogelcj@gmail.com>
2014-02-08 14:26:15 -0500

    basic filtering

--- src/graphs.py
+++ src/graphs.py
@@ -4,6 +4,10 @@ import numpy as np
 import data as dc
 
 d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
+def reject_outliers(data, m):
+        return data[abs(data - np.mean(data)) < m * np.std(data)]
 for i in range(5):
         data = d.getColumn("V" + str(i))
-        print data.std()
+        data2 = reject_outliers(data, 1)
+        print data.max()
+        print data2.max()
