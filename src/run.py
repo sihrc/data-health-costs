3ed0030114fdae65ab1dc05bc9f722899af1f321
Chris <sihrc.c.lee@gmail.com>
2014-02-09 13:33:28 -0500

    Finished Extracting and Plotting Data from FeatureCostRange for cost data in each bin of each feature

--- src/run.py
+++ src/run.py
@@ -5,8 +5,28 @@ import analyze as an
 import visuals as vis
 import data as dc
 
+import os
+
 if __name__ == "__main__":
 	d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
-	vis.AllFeatureVsCost(an.featureCostRange(d))
+
+	"""
+	Extracting and Plotting data from featureCostRange for cost data in each bin of each feature
+	"""
+	for feature, data in an.AllFeatureCostRange(d).iteritems():
+		type_ = data[0]
+		for low,high,data in data[1:]:
+			print data
+			ranges = "_" + str(low) + "-" + str(high)
+			name = d.lookUp(var = feature)[0] + ranges + ".jpg"
+			if len(data) <= 2:
+				print feature + ranges, " does not have multiple data points!"
+				with open(os.path.join("..","visuals","feature_bin_costs", name), 'wb') as f:
+					f.write(feature + "\n")
+					f.write(ranges + "\n")
+					f.write(data)
+				continue
+			vis.GraphCostPmf(data, os.path.join("..","visuals","feature_bin_costs", name), False)
+
 	#print d.getColumn("V2")
 	#vis.AllFeatureVsCost(d)
