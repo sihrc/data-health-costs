0db414347fa80c5c4955423fb8de045fb7520028
CJ Fogel <fogelcj@gmail.com>
2014-02-09 11:51:32 -0500

    changing filename, running through more data

--- src/outliers.py
+++ src/outliers.py
@@ -2,12 +2,13 @@ import matplotlib.pyplot as plt
 from stats import *
 import numpy as np
 import data as dc
+import visuals as vis
 
 d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
 def reject_outliers(data, m):
         return data[abs(data - np.mean(data)) < m * np.std(data)]
-for i in range(5):
+for i in range(44):
         data = d.getColumn("V" + str(i))
         data2 = reject_outliers(data, 1)
-        print data.max()
-        print data2.max()
+        print str(i) + " oldmax " + str(data.max())
+        print "newmax " + str(data2.max())
