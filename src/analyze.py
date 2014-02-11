b84ce95674b7ec5dd86cfd6d0ce69bbb705e1b8d
Chris <sihrc.c.lee@gmail.com>
2014-02-11 16:00:41 -0500

    Graphing Cost and Feature

--- src/analyze.py
+++ src/analyze.py
@@ -13,7 +13,7 @@ import data as dc
 from wrappers import debug
 
 @debug
-def AllFeatureCostRange(d, bins = 10):
+def FeatureCostRange(d,var, mode, bins = 10):
 	"""
 	Returns a dictionary {features: dictionary2}
 	where dicionary2 is {feature ranges: (cost-ranges, count)}
@@ -22,30 +22,28 @@ def AllFeatureCostRange(d, bins = 10):
 
 	author: chris
 	"""
-	#Initiate first Dictionary
-	feature_dicts = dict()
-	#Loop through the different features
-	for i in range(len(d.features)):
-		try:
-			#Grab the relevant data
-			var = "V"+str(i)
-			data = d.getColumn(var)
+	cost = []
+	#try:
+	#Grab the ranges for the feature
+	data = d.getColumn(var).astype("float")
+	if mode:
+		bins = max(data) - min(data) + 1
+	ranges = np.linspace(min(data), max(data), bins) #Unssuport crap
+	cost.append(["classification"] if len(set(data)) < 10 else ["continuous"])
 
-			#Grab the ranges for the feature
-			ranges = np.linspace(min(data), max(data), bins)
+	#Loop through the ranges to get the costs
+	for i,(low, high) in enumerate(zip(ranges[:-1], ranges[1:])):
+		#Grab the costs at those ranges
+		costs = d.cost[np.where((low < data) * (data < high))]
+		cost.append((low, high, costs))
+	#except:
+	#If the data contains non-numeral data, catch the exception
+	#print "data is not numeral"
+	return cost
 
-			feature_dicts[var] = ["classification"] if len(set(data)) < 10 else ["continuous"]
+def reject_outliers(data, m=2):
+    return data[abs(data - np.mean(data)) < m * np.std(data)]
 
-			#Check if Category or Continuous 
-			#Loop through the ranges to get the costs
-			for i,(low, high) in enumerate(zip(ranges[:-1], ranges[1:])):
-				#Grab the costs at those ranges
-				costs = d.cost[np.where((low < data) * (data < high))]
-				feature_dicts[var] += [(low, high, costs)]
-		except:
-			#If the data contains non-numeral data, catch the exception
-			print var + " is not numeral"
-	return feature_dicts
 
 if __name__ == "__main__":
 	d = dc.Data(codebook = dc.HC144D)
