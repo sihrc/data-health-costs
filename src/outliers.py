ea615b24765bd81ae0790e25c3c3bcf286df4a59
Chris <sihrc.c.lee@gmail.com>
2014-02-11 16:09:00 -0500

    moving side-effects codes from outlier into conditional

--- src/outliers.py
+++ src/outliers.py
@@ -4,11 +4,14 @@ import numpy as np
 import data as dc
 import visuals as vis
 
-d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
 def reject_outliers(data, m):
         return data[abs(data - np.mean(data)) < m * np.std(data)]
-for i in range(44):
-        data = d.getColumn("V" + str(i))
-        data2 = reject_outliers(data, 1)
-        print str(i) + " oldmax " + str(data.max())
-        print "newmax " + str(data2.max())
+
+if __name__ == "__main__":
+	d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
+	for i in range(44):
+	        data = d.getColumn("V" + str(i))
+	        data2 = reject_outliers(data, 1)
+	        print str(i) + " oldmax " + str(data.max())
+	        print "newmax " + str(data2.max())
+	
