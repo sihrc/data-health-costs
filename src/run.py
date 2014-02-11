81a1f04f2ec34fd0ea872760e65e1ece0c0669a4
Chris <sihrc.c.lee@gmail.com>
2014-02-11 16:07:54 -0500

    updating run with the original pre-merged state

--- src/run.py
+++ src/run.py
@@ -7,26 +7,31 @@ import data as dc
 import outliers as out
 import os
 
+def GetCostForBinnedFeature(dataL, var):
+	graphData = []
+	print dataL
+	raw_input()
+	for low,high,data in dataL[1:]:
+		an.reject_outliers(data,m=10)
+		ranges = "_" + str(low) + "-" + str(high)
+		name = d.lookUp(var = var)[0] + ranges + ".jpg"
+		name = name.replace(" ","").replace(":","")
+		print data
+		raw_input()
+		if len(data) <= 2:
+			print var + ranges, " does not have multiple data points!"
+			with open(os.path.join("..","visuals","feature_bin_costs", name[:-3] + ".txt"), 'wb') as f:
+				f.write(var + "\n")
+				f.write(ranges + "\n")
+				f.write(data)
+			continue
+		graphData.append((low,high,data))
+		path = os.path.join("..","visuals","feature_bin_costs")
+		if not os.path.exists(path):
+			os.makedirs(path)
+		vis.GraphPmf(data, os.path.join(path, name.replace("/", "")),100, False)
+
 if __name__ == "__main__":
 	d = dc.Data(codebook = dc.HC144D, filename = "h144d.dat")
-
-	"""
-	Extracting and Plotting data from featureCostRange for cost data in each bin of each feature
-	"""
-	for feature, data in an.AllFeatureCostRange(d).iteritems():
-		data = out.rejectoutliers(data, 1)
-		type_ = data[0]
-		for low,high,data in data[1:]:
-			ranges = "_" + str(low) + "-" + str(high)
-			name = d.lookUp(var = feature)[0] + ranges + ".jpg"
-			if len(data) <= 2:
-				print feature + ranges, " does not have multiple data points!"
-				with open(os.path.join("..","visuals","feature_bin_costs", name), 'wb') as f:
-					f.write(feature + "\n")
-					f.write(ranges + "\n")
-					f.write(data)
-				continue
-			vis.GraphCostPmf(data, os.path.join("..","visuals","feature_bin_costs", name.replace("/", "")), False)
-
-	#print d.getColumn("V2")
-	#vis.AllFeatureVsCost(d)
+	GetCostForBinnedFeature(an.FeatureCostRange(d, "V24", True), "V24")
+	#print data
