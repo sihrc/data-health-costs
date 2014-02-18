c7283f5327f05eb70cfefa2bb7a31ce131f64f8c
Chris <sihrc.c.lee@gmail.com>
2014-02-18 15:44:13 -0500

    fixed dictionary loop bug

--- src/run.py
+++ src/run.py
@@ -8,8 +8,6 @@ import os
 
 
 class Config: pass
-
-<<<<<<< HEAD
 if __name__ == "__main__":
 	global d
 	
@@ -18,17 +16,10 @@ if __name__ == "__main__":
 	#datafile, codebook = datasets["Emergency Room Visits"]
 	#datafile = datasets["Prescribed Medicines"]
 	#datafile = datasets["General Demographics"]
-	for datafile, codebook in datasets:
+	for datafile, codebook in datasets.iteritems():
 		d = dc.Data(codebook = codebook, filename = datafile)
 		for row in d.features:
 			if row in ["V43","V49"]:
 				continue
 			costRangeData = an.FeatureCostRange(d, row)
 			vis.GetCostForBinnedFeature(d,costRangeData, row) #gets cost for feature V24
-=======
-	d = dc.Data(codebook = dc.H144D, filename = dataset)
-	GetCostForBinnedFeature(an.FeatureCostRange(d, "V24", True), "V24") #gets cost for feature V24
-	
-	"""What is V24? -CJ """
-	#print data
->>>>>>> 09553cac2a873b3ccf576887619af9f3456e4e5d
