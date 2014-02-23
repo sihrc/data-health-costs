c493dddc1127f143fcb7795cd99daa0b794f7447
Chris <sihrc.c.lee@gmail.com>
2014-02-23 11:14:13 -0500

    Merged

--- src/train.py
+++ src/train.py
@@ -1,12 +1,13 @@
-import format_data as fd
 import data as dc
 from wrappers import debug
 import config
+
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
+
 import pickle as p
 import numpy as np
-import os 
+import os
 
 @debug
 def train(trainFeature, trainCost):
@@ -50,17 +51,9 @@ if __name__ == "__main__":
 	# print testFeature.shape
 	# print testCost.shape
 
+	model = train(trainFeature, trainCost)
+	error = predict(model, testFeature, testCost)
 
-if __name__ == "__main__":
-	percentTrain = 0.8
-	h144aCostData = np.load(os.path.join("..","data", "h144a" + "_" + "dataX" + ".npy"))
-	h144aFeatureData = np.load(os.path.join("..","data", "h144a" + "_" + "dataY" + ".npy"))
+	print error # Currently only around 1000 dollars
 
-	end = int(percentTrain*len(h144aFeatureData))
-	trainCost = h144aCostData[0:end]
-	trainFeature = h144aFeatureData[0:end]
-	testCost = h144aCostData[end:len(h144aCostData)]
-	testFeature = h144aFeatureData[end:len(h144aFeatureData)]
-	model = Train(trainCost, trainFeature)
-	# predicts(model, testCost, testFeature)
 
