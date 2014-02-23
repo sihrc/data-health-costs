2b0a7e89b51525b33ab69f463ffae83337bdd8a6
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-02-23 11:04:50 -0500

    changed train so it uses our data

--- src/train.py
+++ src/train.py
@@ -1,8 +1,10 @@
-import analyze_features as af
+import format_data as fd
 import data as dc
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 import pickle as p
+import numpy as np
+import os 
 
 def Train(trainCost, trainFeature):
 	model = RandomForestRegressor()
@@ -18,9 +20,15 @@ def Predict(model, testCost, testFeature):
 
 
 
-
 if __name__ == "__main__":
+	percentTrain = 0.8
+	h144aCostData = np.load(os.path.join("..","data", "h144a" + "_" + "dataX" + ".npy"))
+	h144aFeatureData = np.load(os.path.join("..","data", "h144a" + "_" + "dataY" + ".npy"))
+
+	end = int(percentTrain*len(h144aFeatureData))
+	trainCost = h144aCostData[0:end]
+	trainFeature = h144aFeatureData[0:end]
+	testCost = h144aCostData[end:len(h144aCostData)]
+	testFeature = h144aFeatureData[end:len(h144aFeatureData)]
 	model = Train(trainCost, trainFeature)
-	predicts(model, testCost, testFeature)
-
-
+	# predicts(model, testCost, testFeature)
