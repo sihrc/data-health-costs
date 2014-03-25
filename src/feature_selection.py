0e3c5dac54806c606e8f852e5af76604e687b56e
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-03-25 13:44:43 -0400

    put the running module code into a seperate function

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -4,6 +4,7 @@ from sklearn.cross_validation import train_test_split
 from sklearn.metrics import explained_variance_score
 from pandas import read_csv
 from operator import itemgetter
+import random
 
 #Local Modules
 from wrappers import debug
@@ -28,6 +29,15 @@ def writeFeatures(features, targetName, datafile):
 			write = "%s#%f\n" % (feature, importance)
 			f.write(write.replace("#", (24 - len(write)) * " "))
 
+def runModel(dataFeatures, targetFeatures, target, panda, d, datafile):
+	#Split the data
+	x_train, x_test, y_train, y_test = train_test_split(dataFeatures, targetFeatures[:,target], test_size=0.15, random_state=42)
+	#Create Models
+	model = train(x_train, y_train)
+	# model = config.get(config.path("..","data",datafile,"model.p"), train, trainFeatures = x_train, targetFeature = y_train)
+	#Sorting and Writing Important Features
+	sortedFeatures = sorted(zip(panda.columns.values, model.feature_importances_), key = itemgetter(1))[::-1]
+	writeFeatures(sortedFeatures, d.costs[target], datafile)
 @debug
 def main(datafile):
 	# Getting Data
@@ -42,15 +52,13 @@ def main(datafile):
 	dataFeatures = panda[[feature for feature in panda.columns.values if feature not in d.costs]].as_matrix().astype("float")
 	targetFeatures = panda[d.costs].as_matrix().astype("float")
 
-	for target in xrange(targetFeatures.shape[1]):
-		#Split the data
-		x_train, x_test, y_train, y_test = train_test_split(dataFeatures, targetFeatures[:,target], test_size=0.15, random_state=42)
-		#Create Models
-		model = train(x_train, y_train)
-		# model = config.get(config.path("..","data",datafile,"model.p"), train, trainFeatures = x_train, targetFeature = y_train)
-		#Sorting and Writing Important Features
-		sortedFeatures = sorted(zip(panda.columns.values, model.feature_importances_), key = itemgetter(1))[::-1]
-		writeFeatures(sortedFeatures, d.costs[target], datafile)
+	# runModel on all cost features
+	# for target in xrange(targetFeatures.shape[1]):
+	# 	runModel(dataFeatures, targetFeatures, target, panda, d, datafile)
+
+	#runModel for one cost feature
+	runModel(dataFeatures, targetFeatures, random.randint(0,targetFeatures.shape[1]), panda, d, datafile)
+
 
 if __name__ == "__main__":
-	main("H147")
+	main("H144D")
