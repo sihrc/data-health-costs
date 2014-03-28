c0d4b4428b61364583538c84085e2d2b89f8b6c5
JazminGonzalez-Rivero <jaz.gonzaro@gmail.com>
2014-03-28 13:49:25 -0400

    split up the main function in feature_selection.py

--- src/feature_selection.py
+++ src/feature_selection.py
@@ -31,18 +31,18 @@ def writeFeatures(features, costTag):
             f.write(write.replace("#", (24 - len(write)) * " "))
 
 @debug
-def runModel(x_train, y_train, costTag, columns):
+def runTrain(x_train, y_train, costTag, columns):
     #Create Models
     # model = train(x_train, y_train)
     model = config.get(config.path(path,"model_%s.p" % costTag), train, trainFeatures = x_train, targetFeature = y_train)
+    return model
+
     #Sorting and Writing Important Features
     try:
         sortedFeatures = sorted(zip(columns, model.coef_), key = itemgetter(1))[::-1]
     except:
         sortedFeatures = sorted(zip(columns, model.feature_importances_), key = itemgetter(1))[::-1]
     writeFeatures(sortedFeatures, costTag)
-    return model
-
 @debug
 def clean(*args):
     import os
@@ -53,20 +53,9 @@ def clean(*args):
             os.chmod(filepath, 0777)
             os.remove(filepath)
             print "Cleaning %s" % filepath
-            
-@debug
-def main():
-    # Clean Past Data
-    clean(\
-        # "codebook",\
-        # "model",\
-        # "csv",\
-        # "costs",\
-        # "feature",\
-        )
 
-    # Getting Data
-    d = dc.Data(datafile)
+@debug
+def readData():
     # Reading Data into a Panda Table
     raw_panda = read_csv(d.csv, delimiter = ",")
     panda = raw_panda._get_numeric_data()
@@ -78,26 +67,50 @@ def main():
     targetFeatures = normalize(panda[d.costs].as_matrix().astype("float"), axis = 0)
 
     x_train, x_test, y_train, y_test = train_test_split(dataFeatures, targetFeatures, test_size=0.15, random_state=42)
+    return  x_train, x_test, y_train, y_test, columns
+
+@debug
+def runModel(x_train, x_test, y_train, y_test,columns):            
     # runModel on all cost features
     results = []
     for target in xrange(y_train.shape[1]):
         mask_train = y_train[:,target] > 0
         mask_test = y_test[:,target] > 0
-        model = runModel(x_train[mask_train,:], y_train[mask_train,target], d.costs[target], columns)
+        model = runTrain(x_train[mask_train,:], y_train[mask_train,target], d.costs[target], columns)
         prediction = predict(model, x_test[mask_test,:], y_test[mask_test,target])
         print prediction
         results.append(prediction)
-
-    print results
-
+    return results
 
     #runModel for one cost feature
     # target = randint(0,y_train.shape[1] - 1)
     # model = runModel(x_train, y_train[:,target], d.costs[target], columns)
     # print predict(model, x_test, y_test[:,target])
 
+@debug
+def main():
+    # Clean Past Data
+    clean(\
+        # "codebook",\
+        # "model",\
+        # "csv",\
+        # "costs",\
+        # "feature",\
+        )
+
+    x_train, x_test, y_train, y_test, columns = readData()
+
+    results =  runModel(x_train, x_test, y_train, y_test, columns)
+
+    print results
+
+
+
 
 if __name__ == "__main__":
     datafile = "H147"
+     # Getting Data
+    d = dc.Data(datafile)
     path = config.path("..","data",datafile)
     main()
+
