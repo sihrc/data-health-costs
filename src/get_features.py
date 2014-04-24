df4c38953d8de2b9da38b284babd7bcba1b30232
Chris <sihrc.c.lee@gmail.com>
2014-04-24 14:48:02 -0400

    Removed comments and unnecessary print statemetns

--- src/get_features.py
+++ src/get_features.py
@@ -44,38 +44,21 @@ def read_tables(datafile):
     end = page.rfind("<a name=\"Appendix")
     soup = Soup(page[:end])
     titles, tables = [], []
-    # print start
-    # print page[:end]
     found_tables = soup.find_all("table", summary= re.compile("This table identifies the variable .*"))
-    # print found_tables
     for table in found_tables:
         title = table.caption
         if title == None:
-            # title = table.parent
             title = table.find_previous_sibling("p", {"class":"contentStyle"})
         titles.append(title.text.encode("utf-8"))
         tables.append([var.text.encode("utf-8") for var in table.find_all("th")[3:]])
-    # raw_input()
-    # found_titles = soup.find_all("p", {"class":"contentStyle"})[2:]
-    # if len(found_titles) == 0:
-    #     found_titles = soup.find_all("caption")
-    #     print found_titles
-    #     for title in found_titles:
-    #         titles.append(title.text.encode("utf-8"))
-    #         tables.append([var.text.encode("utf-8") for var in title.parent.find_all("th")[3:]])
-    # else:
-    #     for title in found_titles:
-    #         titles.append(title.text.encode('utf-8'))
-    #         tables.append([var.text.encode("utf-8") for var in title.find_next_sibling("table").find_all("th")[3:]])
-   
+
+    if not (len(titles) == len(tables) and titles != [] and [] not in tables):
+        return False
+        
     variables = dict(zip(titles,tables))
     print variables
-    if len(titles) == len(tables) and titles != [] and [] not in tables:
-        return True
-    else:
-        print tables
-        print titles
-        return False
+
+    return variables
 
 
 if __name__ == "__main__":
