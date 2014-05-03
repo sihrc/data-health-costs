5e7872a7ba7f83e9384219caa63115b7234346a5
Chris <sihrc.c.lee@gmail.com>
2014-05-02 22:34:29 -0400

    Finished packaging model, now for use

--- src/run.py
+++ src/run.py
@@ -35,7 +35,7 @@ def variable_lookup(datafile, tables):
     return
 
 @debug
-def extract_model(path, datafile, cost):
+def extract_model(path, datafile, cost, d):
     """
     Given the target cost name and data set. Extracts the model to ..\models\model_name\ for use in future
     """
@@ -43,7 +43,20 @@ def extract_model(path, datafile, cost):
     contTags, catTags = config.load(path)
     path = config.path("..","models", cost)
     shutil.copy(config.path("..", "data", datafile, "models", "%s.p" % cost), config.path(path, "%s.p" % cost))
+    data = [d.tags[tag] for tag in contTags] + [d.tags[tag] for tag in catTags]
+
+    with open(config.path(path, "input.csv"), 'wb') as f:
+        f.write(",".join(data))
+
+    with open(config.path(path, "features.txt"), 'wb') as f:
+        f.write("\n".join([tag + "\t" + d.features[tag][1] + "\n\t" + "\n\t".join(["\t==========\t".join(line) for line in d.features[tag][2]]) + "\n" for tag in data]))
+
     sys.exit()
+
+
+@debug
+def use_model(path, datafile):
+    pass
 
 if __name__ == "__main__":
     from optparse import OptionParser
@@ -101,13 +114,17 @@ if __name__ == "__main__":
             ], datafile = options.datafile)
 
     if options.extract != "":
+        d = config.get(config.path("..","data",options.datafile,"data","dHandler.p"), dc.Data, datafile = options.datafile)     
         path = config.path("..","data", options.datafile, "models", "config_%s.p" % options.extract)
         if not os.path.exists(path):
             print "The %s model was not found under %s" % (options.extract.upper(), options.datafile)
             print "Please run \npython run.py -f [data set] -s [features] -c [cost-features] -d -t [number of trees]"
             sys.exit()
-        extract_model(path, options.datafile, options.extract)
+        extract_model(path, options.datafile, options.extract, d)
 
+    if options.model != "":
+        d = config.get(config.path("..","data",options.datafile,"data","dHandler.p"), dc.Data, datafile = options.datafile)     
+        
 
     d = config.get(config.path("..","data",options.datafile,"data","dHandler.p"), dc.Data, datafile = options.datafile)     
     m.main(options.select.strip()[1:-1].split(","), options.costs.strip()[1:-1].split(","), d, include_costs = options.include, trees = int(options.trees))
